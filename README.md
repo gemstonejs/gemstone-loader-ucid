9df4c77ff6a533bdd529d51e53dddb00704ea104
Ralf S. Engelschall <rse@engelschall.com>
2017-04-08 20:02:11 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gemstone-loader-ucid
-Loader for Unique Component Identifiers (UCID)
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Loader Unique Component Identifier (UCID)
+==================================================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-loader-ucid.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-loader-ucid.png" alt=""/>
+
+About
+-----
+
+This is the Webpack Loader for injecting Unique Component Identifier (UCID) in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-loader-ucid.js
@@ -0,0 +1,46 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load internal requirements  */
+const path        = require("path")
+const crypto      = require("crypto")
+
+/*  load external requirements  */
+const loaderUtils = require("loader-utils")
+const base58      = require("base-58")
+
+/*  the exported Webpack loader function  */
+module.exports = function (content) {
+    /*  determine Webpack loader query parameters  */
+    const options = Object.assign({}, {
+        sourceDir:  ".",
+        idMatch:    "__ucid",
+        idReplace:  "ucid<ucid>"
+    }, loaderUtils.getOptions(this), this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null)
+
+    /*  indicate to Webpack that our results are
+        fully deterministic and can be cached  */
+    this.cacheable(true)
+
+    /*  determine filesystem path of content,
+        relative to source directory and without any extensions  */
+    let relPath = path.relative(path.resolve(options.sourceDir), path.resolve(this.resourcePath))
+    relPath = relPath.replace(/([^\\/.]+)(?:\.[^\\/.]+)+$/, "$1")
+
+    /*  determine syntactically harmless id from filesystem path  */
+    const hash = crypto.createHash("md5")
+    hash.update(relPath)
+    let ucid = base58.encode(hash.digest())
+
+    /*  inject */
+    let match   = new RegExp(options.idMatch, "g")
+    let replace = options.idReplace.replace(/<ucid>/, ucid)
+    content = content.replace(match, replace)
+
+    /*  return the resulting content  */
+    return content
+}
+
